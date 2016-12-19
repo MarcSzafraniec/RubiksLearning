3ec8c1394f0042aaf10ac0bc2d949d161cc3d4d4
MarcSzafraniec <mszafraniec@laposte.net>
2016-12-19 19:01:13 +0100

    Added experience replay but too many oscillations, need target network

--- DQN.py
+++ DQN.py
@@ -5,6 +5,7 @@
 
 import sys
 sys.path.insert(0, 'Resources/MagicCube/code/')
+import os
 import matplotlib.pyplot as plt
 import random as rd
 from bitstring import BitArray
@@ -105,68 +106,78 @@ nb_actions = len(actions)
 
 c_init=Cube(3)
 
-resume = sys.argv[1] == "True"
+#==============================================================================
+#                           DEFINE NEURAL NETWORK
+#==============================================================================
 
-# sess = tf.InteractiveSession()
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) 
-with tf.device("/gpu:0"):
+ resume = sys.argv[1] == "True"
+ 
+ # sess = tf.InteractiveSession()
+ sess = tf.Session(config=tf.ConfigProto(log_device_placement=True)) 
+ with tf.device("/gpu:0"):
+ 
+     x = tf.placeholder(tf.float32, shape=[None,6*c_init.N**2])
+     # act = tf.placeholder(tf.float32, shape=[nb_actions,None])
+     # Q_ = tf.placeholder(tf.float32, shape=[None,1])
+     Q_ = tf.placeholder(tf.float32, shape=[None,nb_actions])
+ 
+ 
+     if not resume:
+         W1 = tf.Variable(tf.random_normal([6*c_init.N**2,5000], stddev=1e-2))
+         # b1 = tf.Variable(tf.random_normal([6*c_init.N**2], stddev=1e-6))
+ 
+         W2 = tf.Variable(tf.random_normal([5000,nb_actions], stddev=1e-2))
+         # b2 = tf.Variable(tf.random_normal([nb_actions], stddev=1e-6))  
+     else:
+         load = pickle.load(open('save.p', 'rb'))
+         W1 = tf.Variable(load[0])
+         W2 = tf.Variable(load[1])
+ 
+ 
+     Q1 = tf.matmul(x/6,W1)# + b1
+     Qs1 = tf.nn.tanh(Q1)
+     Q2 = tf.matmul(Qs1,W2)#tf.nn.relu(tf.matmul(Qs1,W2))# + b2)
+     # Qs = tf.matmul(Q2,act)
+     Qs = Q2
+ 
+     
+     loss_function = tf.reduce_mean(tf.square(tf.sub(Q_,Qs)))
+ 
+     train_step = tf.train.AdamOptimizer(1e-4).minimize(loss_function)
+     
+     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+     sess.run(init_op)
+ 
+ D = []
+#==============================================================================
 
-    x = tf.placeholder(tf.float32, shape=[None,6*c_init.N**2])
-    # act = tf.placeholder(tf.float32, shape=[nb_actions,None])
-    # Q_ = tf.placeholder(tf.float32, shape=[None,1])
-    Q_ = tf.placeholder(tf.float32, shape=[None,nb_actions])
-
-
-    if not resume:
-        W1 = tf.Variable(tf.random_normal([6*c_init.N**2,5000], stddev=1e-2))
-        # b1 = tf.Variable(tf.random_normal([6*c_init.N**2], stddev=1e-6))
-
-        W2 = tf.Variable(tf.random_normal([5000,nb_actions], stddev=1e-2))
-        # b2 = tf.Variable(tf.random_normal([nb_actions], stddev=1e-6))  
-    else:
-        load = pickle.load(open('save.p', 'rb'))
-        W1 = tf.Variable(load[0])
-        W2 = tf.Variable(load[1])
-
-
-    Q1 = tf.matmul(x/6,W1)# + b1
-    Qs1 = tf.nn.tanh(Q1)
-    Q2 = tf.matmul(Qs1,W2)#tf.nn.relu(tf.matmul(Qs1,W2))# + b2)
-    # Qs = tf.matmul(Q2,act)
-    Qs = Q2
-
-    
-    loss_function = tf.reduce_mean(tf.square(tf.sub(Q_,Qs)))
-
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss_function)
-    
-    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-    sess.run(init_op)
-
-D = []
+#==============================================================================
 
 
 # In[37]:
 
 def DQN(c_init,Tmax,nb_episodes, n_moves):
     
+    plt.ion()
     done = 0
     lActions = np.zeros(18)
-    print("moves","\t","ep.","\t","Loss Function","\t","Min Q","\t\t", "Reward", "", "NB.","\t", "Prcent.")
+    print("moves","\t","ep.","\t","Loss Function","\t","Min Q","\t\t", "Reward", "", "NB.","\t", "Prcent.","\t","Mn. Prcent.")
     
     mineps = .1
     def eps(episode):
         return min(1,max(.1,100/(1+episode)))
     
-    lenBatch = 10
+    lenBatch = 10*Tmax
     
     episode = 1
+    
+    percentDone = []
     
     tries = 1
     
     dones = np.empty([0])
     
-    while np.sum(dones[-1000:])/min(1000,tries) < 1 - mineps and episode < nb_episodes:  
+    while np.sum(dones[-1000:])/min(1000,tries) < .8 and episode < nb_episodes:  
         
         episode += 1
         
@@ -187,7 +198,7 @@ def DQN(c_init,Tmax,nb_episodes, n_moves):
                 a = np.argmax(Qout)
             else:
                 a = rd.randint(0,nb_actions-1)
-
+        
             lActions[a] += 1
             
             #print(actions[a])
@@ -198,23 +209,25 @@ def DQN(c_init,Tmax,nb_episodes, n_moves):
             s.move(f,l,d)            
             r = reward_cube(s)
             cum_reward.append(r)
-            D.append(copy.copy([S, a, r, np.reshape(s.stickers,(1, 54))]))
+            D.append(copy.copy([S, a, r, np.reshape(s.stickers,(1, 54)), numCompleteFaces(s)]))
             
             #print(S)
             #print(np.reshape(s.stickers,(1, 54)))
             
-            #Obtain the Q' values by feeding the new state through our network
-            Qprime = sess.run(Q2,feed_dict={x:np.reshape(s.stickers,(1, 54))})
-            #Obtain maxQ' and set our target value for chosen action.
-            maxQprime = np.max(Qprime)
-            targetQ = Qout
-            targetQ[0,a] = r + gamma*maxQprime
-            #Train our network using target and predicted Q values
-               
-            sess.run(train_step,feed_dict={Q_: targetQ, x: S})
+#==============================================================================
+#             #Obtain the Q' values by feeding the new state through our network
+#             Qprime = sess.run(Q2,feed_dict={x:np.reshape(s.stickers,(1, 54))})
+#             #Obtain maxQ' and set our target value for chosen action.
+#             maxQprime = np.max(Qprime)
+#             targetQ = Qout
+#             targetQ[0,a] = r + gamma*maxQprime
+#             #Train our network using target and predicted Q values
+#                
+#             sess.run(train_step,feed_dict={Q_: targetQ, x: S})
+#==============================================================================
             
             #print(targetQ)
-
+        
             cum_reward.append(r)
             
             
@@ -225,49 +238,61 @@ def DQN(c_init,Tmax,nb_episodes, n_moves):
         dones = np.append(dones,done)
             
         
-#==============================================================================
-#         if episode%lenBatch == 0:
-#             Dshuf = D
-#             random.shuffle(Dshuf)
-#             batch = np.array(Dshuf[:lenBatch*Tmax])
-# 
-#             tts = np.empty([0,nb_actions])
-# 
-#             for i in range(len(batch)):
-# 
-#                 faces_done = np.sum([np.sum([batch[i][-1][0][f*9+j] != batch[i][-1][0][f*9] for j in range(9)]) == 0 for f in range(6)])
-#                 
-#                 Qprime = sess.run(Q2,feed_dict={x:batch[i][-1]})
-#                 maxQprime = np.max(Qprime)
-#                 
-#                 tt = sess.run(Q2,feed_dict={x:batch[i][0]})
-#                 if faces_done == 6:
-#                     tt[0,batch[i][1]] = batch[i][-2]
-#                 else:
-#                     tt[0,batch[i][1]] = batch[i][-2] + gamma*maxQprime
-# 
-#                 tts = np.concatenate((tts,tt),0)
-# 
-#             sess.run(train_step,feed_dict={Q_: tts, x: batch[:,0][0]})
-#==============================================================================
+# ============================================================================== 
+#                           EXPERIENCE REPLAY      
+# ==============================================================================
+
+        if episode%lenBatch == 0:
+              Dshuf = D[-lenBatch:]
+              random.shuffle(Dshuf)
+#              batch = np.array(Dshuf[:lenBatch])
+              batch = np.array(Dshuf)
+              
+              tts = np.empty([0,nb_actions])
+              
+              for i in range(len(batch)):
+              
+                  faces_done = batch[i][-1]
+                  
+                  Qprime = sess.run(Q2,feed_dict={x:batch[i][-2]})
+                  maxQprime = np.max(Qprime)
+                  
+                  tt = sess.run(Q2,feed_dict={x:batch[i][0]})
+                  if faces_done == 6:
+                      tt[0,batch[i][1]] = batch[i][-3]
+                  else:
+                      tt[0,batch[i][1]] = batch[i][-3] + gamma*maxQprime
+              
+                  tts = np.concatenate((tts,tt),0)
+              
+              sess.run(train_step,feed_dict={Q_: tts, x: batch[:,0][0]})
+              
+# ============================================================================== 
+#                           
+# ==============================================================================
             
-        if episode%10 == 1:
+
+        if episode%100 == 1:
 #             sess.run(loss_function,feed_dict={Q_: targetQ, x: S}),"\t",
-            print(n_moves,"\t",episode,"\t",min(sess.run(Q2,feed_dict={x:S})[0]),"\t", round(np.mean(cum_reward[-1]),2), "\t", np.sum(dones[-1000:]),"\t", round(100*np.sum(dones[-1000:])/min(1000,tries),2))
+            print(n_moves,"\t",episode,"\t",min(sess.run(Q2,feed_dict={x:S})[0]),"\t", round(np.mean(cum_reward[-1]),2), "\t", np.sum(dones[-1000:]),"\t", round(100*np.sum(dones[-1000:])/min(1000,tries),2),"\t", round(100*np.sum(dones)/tries,2))
+            percentDone.append(100*np.sum(dones[-1000:])/min(1000,tries))
     #             print(lActions)
-            print(np.var(sess.run(Q2,feed_dict={x:S})))
+#            print(np.var(sess.run(Q2,feed_dict={x:S})))
         
         if episode%1000 == 1:
+            plt.clf()
+            plt.plot(percentDone, linewidth = 2)
+            plt.title("n_moves: "+str(n_moves))
+            plt.pause(0.0001)
             topickle = [sess.run(W1),sess.run(W2)]
             pickle.dump(topickle, open('save.p', 'wb'))
 
 
 
-
             
-def longTrain(c_init,n_moves_max):
+def longTrain(c_init,n_moves_init, n_moves_max):
 
-    for i in range(1,1+n_moves_max):
+    for i in range(n_moves_init,1+n_moves_max):
         print("==============================================================================")
         print("\t",i,"Moves","\t")
         print("==============================================================================")
@@ -276,7 +301,7 @@ def longTrain(c_init,n_moves_max):
 # In[ ]:
 
 with tf.device("/gpu:0"):
-    longTrain(Cube(3),10)
+    longTrain(Cube(3),1,10)
 #    DQN(c_init=Cube(3),Tmax=int(sys.argv[4]),nb_episodes=int(sys.argv[2]),n_moves = int(sys.argv[3]))
 
 
@@ -284,7 +309,6 @@ with tf.device("/gpu:0"):
 
 topickle = [sess.run(W1),sess.run(W2)]
 pickle.dump(topickle, open('save.p', 'wb'))
-
 
 # In[ ]:
 
